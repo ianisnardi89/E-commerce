ecb9e779c29e516efa85d531153cbaa547de0235
ISNARDI BIEBER IAN <ianisnardi89@gmail.com>
2021-09-04 00:58:59 -0300

    C0008 - Añadido de UsuarioService

---
+++ src/main/java/com/informatorio/ejemplo/service/UsuarioService.java
@@ -0,0 +1,41 @@
+package com.informatorio.ejemplo.service;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+import com.informatorio.ejemplo.entity.Usuario;
+import com.informatorio.ejemplo.repository.UsuarioRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+    
+    @Autowired
+    private static UsuarioRepository usuarioRepository;
+
+    public static Date creacion(){
+        return Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static Usuario modificarCredenciales(Usuario user, Usuario usuario){
+        user.setPass(usuario.getPass());
+        user.setEmail(usuario.getEmail());
+        return usuarioRepository.save(user);
+    }
+
+    public static Usuario modificarDatos(Usuario user, Usuario usuario){
+        user.setNombre(usuario.getNombre());
+        usuario.setApellido(usuario.getApellido());
+        usuario.setDireccion(usuario.getDireccion());
+        return usuarioRepository.save(user);
+    }
+
+
+
+
+
+
+}
